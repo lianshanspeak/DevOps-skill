de3142d49d139d7a9b52fc4080340a212bac7481
lianshan <ly201552y@163.com>
2022-11-16 23:44:53 +0800

    网卡流量监控

---
+++ shell_collect/interface.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NIC=$1
+echo -e "In~~~~~~~~~~~~~out"
+
+while true;do
+  OLD_IN=$(awk '$0~"'$NIC'"{print $2}' /proc/net/dev)
+  OLD_OUT=$(awk '$0~"'$NIC'"{print $10}' /proc/net/dev)
+  sleep 1
+  NEW_IN=$(awk '$0~"$NIC"{print $2}' /proc/net/dev)
+  NEW_OLD=$(awk '$0~"$NIC"{print $10}' /proc/net/dev)
+  IN=$(printf "%.lf%s" "$((($NEW_IN-$OLD_IN")/1024)" "KB/S")
+  OUT=$(printf "%.lf%s" "$((($NEW_OUT-$OLD_OUT)/1024)" "KB/S")
+  echo "$IN $OUT"
+  sleep 1
+done
+
