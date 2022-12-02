89a574112232d3b602f8bca9f7e7df9caae9fa0d
lianshan <ly201552y@163.com>
2022-12-02 23:51:48 +0800

    服务器利用率检查，这个比较实用

--- shell_collect/interface.sh
+++ shell_collect/interface.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#使用方法bash interface.sh eth0
 NIC=$1
 echo -e "In~~~~~~~~~~~~~out"
 
@@ -6,10 +7,10 @@ while true;do
   OLD_IN=$(awk '$0~"'$NIC'"{print $2}' /proc/net/dev)
   OLD_OUT=$(awk '$0~"'$NIC'"{print $10}' /proc/net/dev)
   sleep 1
-  NEW_IN=$(awk '$0~"$NIC"{print $2}' /proc/net/dev)
-  NEW_OLD=$(awk '$0~"$NIC"{print $10}' /proc/net/dev)
-  IN=$(printf "%.lf%s" "$((($NEW_IN-$OLD_IN")/1024)" "KB/S")
-  OUT=$(printf "%.lf%s" "$((($NEW_OUT-$OLD_OUT)/1024)" "KB/S")
+  NEW_IN=$(awk '$0~"'$NIC'"{print $2}' /proc/net/dev)
+  NEW_OLD=$(awk '$0~"'$NIC'"{print $10}' /proc/net/dev)
+  IN=$(printf "%.lf%s" "$((($NEW_IN-$OLD_IN)/1024))" "KB/S")
+  OUT=$(printf "%.lf%s" "$((($NEW_OUT-$OLD_OUT)/1024))" "KB/S")
   echo "$IN $OUT"
   sleep 1
 done
